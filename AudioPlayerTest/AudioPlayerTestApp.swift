2785a43b9b8e9889a5150d483e63f6c85ddbf54b
Muhammad Zeeshan <mzeeshanid@yahoo.com>
2026-08-31 22:39:29 +0500

    Initial Commit

---
+++ AudioPlayerTest/AudioPlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerTestApp.swift
+//  AudioPlayerTest
+//
+//  Created by Zeeshan on 31/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioPlayerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioPlayerTest
+//
+//  Created by Zeeshan on 31/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
